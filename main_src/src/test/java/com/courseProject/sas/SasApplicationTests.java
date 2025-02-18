8ea7ccae165d7884a0ad765fc20d77c4178f42f2
Sirajum Munir <sirajummunir1102@gmail.com>
2025-02-18 19:33:17 +0600

    Add All The setup

---
+++ main_src/src/test/java/com/courseProject/sas/SasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.courseProject.sas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
